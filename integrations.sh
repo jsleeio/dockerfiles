ac3de092f739cd3fb9f381eca08fff527138279e
John Slee <john@sleefamily.org>
2022-04-14 20:32:55 +1000

    tech debt cleanup, +rpi-pico-builder
    Haven't used most of these in ages. Bin them.
    
    Also add a builder container for Raspberry Pi Pico microcontrollers.

--- integrations.sh
+++
@@ -1,6 +0,0 @@
-# bash aliases
-
-jsleeio_containers_root=gcr.io/jsleeio-containers
-
-alias aws="docker run --interactive --tty --rm --volume=$HOME/.aws:/home/user/.aws $jsleeio_containers_root/awscli:latest"
-alias man-linux="docker run --interactive --tty --rm $jsleeio_containers_root/man:latest"
